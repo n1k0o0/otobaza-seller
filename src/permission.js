cc1da43f94eceaa11cca2be217241305b4bcd9dd
Nicat Ismayilov <n1ko.net.dev@gmail.com>
2022-02-06 16:23:06 +0400

    login (fix) login from otobaza (another domain) and check token beforeRoute

--- src/permission.js
+++ src/permission.js
@@ -3,14 +3,14 @@ import store from './store'
 import { Message } from 'element-ui'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
+import { getToken, setToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 const whiteList = ['/login', '/register'] // no redirect whitelist
 
-router.beforeEach(async(to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   // start progress bar
   NProgress.start()
 
@@ -18,7 +18,13 @@ router.beforeEach(async(to, from, next) => {
   document.title = getPageTitle(to.meta.title)
 
   // determine whether the user has logged in
-  const hasToken = getToken()
+  let hasToken = getToken()
+
+  if (to.query.token && hasToken !== to.query.token) {
+    setToken(to.query.token)
+    hasToken = to.query.token
+    await store.commit('user/SET_TOKEN', to.query.token)
+  }
 
   if (hasToken) {
     if (to.path === '/login') {
